9cda6cc9f29efc003c8d507107e3ce9d6b86bbe5
Jaminur Rashid <76808239+Jaminur-Rashid@users.noreply.github.com>
2021-09-12 03:57:42 +0600

    Delete AutoList.js

--- src/Components/AutoList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const AutoList = ({ autos }) => {
-    return autos.map(a => 
-    <div>
-      <h6>{a.organization}</h6>
-      <h6>{a.maxRating}</h6>
-    </div>);
-  };
-  export default AutoList;
